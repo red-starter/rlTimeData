b6bc8c03db950c8919fd0863c01b3de9e05b8ea8
Hack Reactor Students <accounts@hackreactor.com>
2015-09-22 12:33:01 -0700

    building graph, axis our interactive now

--- client/app/graph/graph.js
+++ client/app/graph/graph.js
@@ -15,23 +15,83 @@ angular.module('app.graph',[])
 	// console.log(modelPropertyNames)
 	// console.log($scope.userModel);
 	//$scope.xaxis
-	var xAxisArr = _.pluck($scope.userInput,'population')
-	var yAxisArr = _.pluck($scope.userInput,'GDP')
 	var sizeArr = _.pluck($scope.userInput,'Life exp.')
 	var opacityArr = _.pluck($scope.userInput,'population')
 	var colorArr = _.pluck($scope.userInput,'countryName')
 
 	$scope.genModel = function(){
 		//used to scale the graph
-		if (xAxisArr && yAxisArr){
-			initGraph(xAxisArr,yAxisArr)
-		}
+		// if (xAxisArr && yAxisArr){
+		// 	initGraph(xAxisArr,yAxisArr)
+		// }
+		initAxis()
+	}
+	//create svg
+	var options ={
+		width:600,
+		height:600,
+		margin:50
+	}
+	//svg selector 
+	var svg = d3.select("#graph").append("svg")
+	.attr("width",options.width)
+	.attr("height",options.height)
+
+	var initAxis = function(){
+		var xAxisArr = _.pluck($scope.userInput,$scope.xaxis) 
+		var yAxisArr = _.pluck($scope.userInput,$scope.yaxis) 
+
+		//scale x coordinate to fit into svg element, mapX and mapY are functions
+		var mapX = d3.scale.linear().domain([d3.min(xAxisArr),d3.max(xAxisArr)]).range([options.margin,options.width-options.margin]);
+		var mapY = d3.scale.linear().domain([d3.min(yAxisArr),d3.max(yAxisArr)]).range([options.height - options.margin,options.margin]);
+		
+		var xAxis = d3.svg.axis()
+		.scale(mapX) //where to orient numbers
+		.orient('bottom') 
+
+		var yAxis = d3.svg.axis()
+		.scale(mapY)
+		.orient('left')
+
+		//clear previous append
+		svg.selectAll("g").remove()
+		svg.selectAll(".h").remove()
+		svg.selectAll(".v").remove()
+
+		// use svg selector, add axis to svg as a collection of g elements
+		svg.append("g")
+		.attr('class','axis')
+		.attr('transform','translate(0,'+ (options.height- options.margin) +')')
+		.call(xAxis)
+		// call with the yAxis function
+		svg.append("g")
+		  .attr("class", "axis")
+		  .attr("transform", "translate(" + (options.margin) + ",0)")
+		  .call(yAxis);
+		//fix later
+		//create horizontal lines
+		svg.selectAll(".h").data(d3.range(-8,10,1)).enter()
+		  .append("line").classed("h",1)
+		  .attr("x1",options.margin).attr("x2",options.height-options.margin)
+		  .attr("y1",mapY).attr("y2",mapY)
+		//create vertical lines
+		svg.selectAll(".v").data(d3.range(1,5)).enter()
+		  .append("line").classed("v",1)
+		  .attr("y1",options.margin).attr("y2",options.width-options.margin)
+		  .attr("x1",mapX).attr("x2",mapX)	
 	}
 
-	var initGraph = function(){
+	var initGraph = function(xAxisArr,yAxisArr,sizeArr,opacityArr,colorArr){
+
+		//defaults
+		//scale x coordinate to fit into svg element, mapX and mapY are functions
+		xAxisArr = xAxisArr || [0,10];
+		yAxisArr = yAxisArr || [0,10];
+		xAxisArr = xAxisArr || [0,10];
+
 		var options ={
-			width:500,
-			height:500,
+			width:600,
+			height:600,
 			margin:50
 		}
 		//svg selector 
@@ -80,7 +140,7 @@ angular.module('app.graph',[])
 		  .attr("y1",options.margin).attr("y2",options.width-options.margin)
 		  .attr("x1",mapX).attr("x2",mapX)	
 
-		svg.selectAll("circle").data($scope.userInput,function(e,i){ console.log(e); return e['country']})
+		svg.selectAll("circle").data($scope.userInput,function(e,index){ return index})
 			.enter()
 			.append("circle")
 			//plotting GERD against growth, r is population,+converts to number,need mapping functio 
@@ -93,7 +153,7 @@ angular.module('app.graph',[])
 			.text(function(d){return d['population'] + d['country']})
 	}
 
-	initGraph();
+	// initGraph();
 })
 //tell d3.csv function location of a csv file, 
 // and a function will be run on the array of objects creted by using
